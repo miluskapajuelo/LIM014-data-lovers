667a04572ce330953a838393015fde48bf77c146
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-02-08 16:47:20 -0500

    actualización

--- src/main.js
+++ src/main.js
@@ -37,12 +37,14 @@ data.pokemon.sort(function (prev,next){
     return 0;
 });
 
-let a = filtrara.sortdata(data);
+/* let a = filtrara.sortdata(data);
 for(let i=0;i<a[1].length;i++){
     let node = document.createElement("figure");
     let node2 = document.createElement("img");
     let node3 = document.createElement("figcaption");
     let animacion = document.createElement("div");
+    let node5 = document.createElement("p");
+    let node4 = document.createElement("a");
     animacion.className = 'capa';
     node5.innerText = a[2][i];
     node4.innerText = a[3][i];
@@ -54,16 +56,46 @@ for(let i=0;i<a[1].length;i++){
     figura.appendChild(animacion);
     animacion.appendChild(node4);
     animacion.appendChild(node5);
-}
-   
+} */
 
 
-/* var pokemon = total
-const pokemones = document.getElementById("lista").appendChild(pokemon) */
+const text = document.querySelector("#text");
+const lista = document.getElementById("lista");
+const filtrar = () => {
+   /*  console.log(text.value); */
+   lista.innerHTML = "";
+   const valorTexto = text.value.toLowerCase();
+   let a = filtrara.sortdata(data);
+   for(let i=0;i<a[1].length;i++){
+   let nombre = a[1][i].toLowerCase();
+        if(nombre.indexOf(valorTexto) !==-1){            
+            let node = document.createElement("figure");
+            let node2 = document.createElement("img");
+            let node3 = document.createElement("figcaption");
+            let animacion = document.createElement("div");
+            let node5 = document.createElement("p");
+            let node4 = document.createElement("a");
+            animacion.className = 'capa';
+            node5.innerText = a[2][i];
+            node4.innerText = a[3][i];
+            node2.src = a[0][i];
+            node3.innerText = a[1][i];
+            let figura= document.getElementById("lista").appendChild(node);
+            figura.appendChild(node2);
+            figura.appendChild(node3);
+            figura.appendChild(animacion);
+            animacion.appendChild(node4);
+            animacion.appendChild(node5);
+        }
+    if(lista.innerHTML === ""){
+        lista.innerHTML += ""
+    }}};
 
+    text.addEventListener("keyup", filtrar);
+    filtrar();
+    
 
-
-
+/* button.addEventListener("click", filtrar); */
 
 /*  const enter = document.getElementById("enter");
 enter.addEventListener("click", ()=>{
@@ -76,9 +108,9 @@ enter.addEventListener("click", ()=>{
     document.body.style.background = "#fff";
 }); 
  */
-/* const menu = document.getElementById("menu");
+const menu = document.getElementById("menu");
 menu.addEventListener("click", function press2(){
     let siteNav = document.getElementById("site-nav");
         siteNav.classList.toggle("site-nav-open");
         menu.classList.toggle("menu-open");
-})*/
+});
