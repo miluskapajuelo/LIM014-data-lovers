4bc966f9c0acc3d1b473ffdf83c4be8e515cd09e
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-02-18 17:43:12 -0500

    se agregan iconos al modal cuando utilizamos alguna funcionalidad

--- src/data.js
+++ src/data.js
@@ -64,28 +64,113 @@ const funciones = {
         if (b.name.toLowerCase() > a.name.toLowerCase()) return 1;
         return 0;
       });
-      return data.pokemon;
+      
+      let newArray = [];
+      data.pokemon.forEach(function (pokemones) {
+      /* console.log(pokemones.evolution["next-evolution"]); */
+      newArray.push({
+        img: pokemones.img,
+        name: pokemones.name,
+        about: pokemones.about,
+        num: pokemones.num,
+        type: pokemones.type,
+        weaknesses: pokemones.weaknesses,
+        resistant: pokemones.resistant,
+        generation: pokemones.generation["name"],
+        height: pokemones.size["height"],
+        weight: pokemones.size["weight"],
+        baseAttack: pokemones.stats["base-attack"],
+        baseDefense: pokemones.stats["base-defense"],
+        baseStamina: pokemones.stats["base-stamina"],
+        maxCp: pokemones.stats["max-cp"],
+        maxHp: pokemones.stats["max-hp"],
+      });
+    })
+    return newArray;
     } else if (sortOrder === "Descendente") {
       data.pokemon.sort((a, b) => {
         if (b.name.toLowerCase() < a.name.toLowerCase()) return -1;
         if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
         return 0;
       });
-      return data.pokemon;
+      let newArray = [];
+      data.pokemon.forEach(function (pokemones) {
+      /* console.log(pokemones.evolution["next-evolution"]); */
+      newArray.push({
+        img: pokemones.img,
+        name: pokemones.name,
+        about: pokemones.about,
+        num: pokemones.num,
+        type: pokemones.type,
+        weaknesses: pokemones.weaknesses,
+        resistant: pokemones.resistant,
+        generation: pokemones.generation["name"],
+        height: pokemones.size["height"],
+        weight: pokemones.size["weight"],
+        baseAttack: pokemones.stats["base-attack"],
+        baseDefense: pokemones.stats["base-defense"],
+        baseStamina: pokemones.stats["base-stamina"],
+        maxCp: pokemones.stats["max-cp"],
+        maxHp: pokemones.stats["max-hp"],
+      });
+    })
+    return newArray;
     } else if (sortOrder === "Debilidad") {
       data.pokemon.sort((a, b) => {
         if (a.weaknesses < b.weaknesses) return -1;
         if (b.weaknesses > a.weaknesses) return 1;
         return 0;
       });
-      return data.pokemon;
+      let newArray = [];
+      data.pokemon.forEach(function (pokemones) {
+      /* console.log(pokemones.evolution["next-evolution"]); */
+      newArray.push({
+        img: pokemones.img,
+        name: pokemones.name,
+        about: pokemones.about,
+        num: pokemones.num,
+        type: pokemones.type,
+        weaknesses: pokemones.weaknesses,
+        resistant: pokemones.resistant,
+        generation: pokemones.generation["name"],
+        height: pokemones.size["height"],
+        weight: pokemones.size["weight"],
+        baseAttack: pokemones.stats["base-attack"],
+        baseDefense: pokemones.stats["base-defense"],
+        baseStamina: pokemones.stats["base-stamina"],
+        maxCp: pokemones.stats["max-cp"],
+        maxHp: pokemones.stats["max-hp"],
+      });
+    })
+    return newArray;
     } else if (sortOrder === "Resistencia") {
       data.pokemon.sort((a, b) => {
         if (b.resistant > a.resistant) return -1;
         if (a.resistant < b.resistant) return 1;
         return 0;
       });
-      return data.pokemon;
+      let newArray = [];
+      data.pokemon.forEach(function (pokemones) {
+      /* console.log(pokemones.evolution["next-evolution"]); */
+      newArray.push({
+        img: pokemones.img,
+        name: pokemones.name,
+        about: pokemones.about,
+        num: pokemones.num,
+        type: pokemones.type,
+        weaknesses: pokemones.weaknesses,
+        resistant: pokemones.resistant,
+        generation: pokemones.generation["name"],
+        height: pokemones.size["height"],
+        weight: pokemones.size["weight"],
+        baseAttack: pokemones.stats["base-attack"],
+        baseDefense: pokemones.stats["base-defense"],
+        baseStamina: pokemones.stats["base-stamina"],
+        maxCp: pokemones.stats["max-cp"],
+        maxHp: pokemones.stats["max-hp"],
+      });
+    })
+    return newArray;
     }
   },
 };
