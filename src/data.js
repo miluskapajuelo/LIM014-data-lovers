b4d70d3a3dcf796092cdf771fea505647b97c3e6
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-02-18 09:23:15 -0500

    se mejora código javascript

--- src/data.js
+++ src/data.js
@@ -1,96 +1,73 @@
-const funciones={ 
- /*  FILTRO POKEMON */
-  FilterData : function(data, tipo){
-    let PokemonesFiltrados = data.pokemon.filter(data => data.type == tipo);
-    let newArray = [] 
-    PokemonesFiltrados.forEach(function(pokemones){
-      
+const funciones = {
+  /*  FILTRO POKEMON */
+  FilterData: function (data, tipo) {
+    let PokemonesFiltrados = data.pokemon.filter((data) => data.type == tipo);
+    let newArray = [];
+    PokemonesFiltrados.forEach(function (pokemones) {
+      console.log(pokemones.evolution["next-evolution"]);
       newArray.push({
-        "img" : pokemones.img,
-        "name" : pokemones.name,
-        "about" : pokemones.about,
-        "num" : pokemones.num, 
-        "type" : pokemones.type,
-        "weaknesses" : pokemones.weaknesses,
-        "generation" : pokemones.generation['name'],
-        "height": pokemones.size['height'],
-        "weight": pokemones.size['weight']
-        
-      })
-          
-    })
-   
-    return newArray
+        img: pokemones.img,
+        name: pokemones.name,
+        about: pokemones.about,
+        num: pokemones.num,
+        type: pokemones.type,
+        weaknesses: pokemones.weaknesses,
+        resistant: pokemones.resistant,
+        generation: pokemones.generation["name"],
+        height: pokemones.size["height"],
+        weight: pokemones.size["weight"],
+        baseAttack: pokemones.stats["base-attack"],
+        baseDefense: pokemones.stats["base-defense"],
+        baseStamina: pokemones.stats["base-stamina"],
+        maxCp: pokemones.stats["max-cp"],
+        maxHp: pokemones.stats["max-hp"],
+      });
+    });
 
+    return newArray;
   },
 
-  /*  FILTRO TEXTO POKEMON */
-  FilterText : function(data, nombre){
-    let PokemonesFiltrados = data.pokemon.filter(data => data.name == nombre);
-    let newArray = [] 
-    PokemonesFiltrados.forEach(function(pokemones){
-      newArray.push({
-        "img" : pokemones.img,
-        "name" : pokemones.name,
-        "about" : pokemones.about,
-        "number" : pokemones.num, 
-        "type" : pokemones.type      
-      })}); return newArray,
-      console.log(newArray)
-    },
-      
-
-  /* TODOS LOS POKEMONES */
-
-  AllData : function(data){
-    let imagen =[];
-    let nombre =[];
-    let about = [];
-    let number = [];
-    data.pokemon.forEach(function(task){
-    if(task.type){
-        imagen.push(task.img)
-        nombre.push(task.name)
-        about.push(task.about)
-        number.push(task.num)
-        }
-    });
-    let bebe = [imagen,nombre,about,number]
-    return bebe
+  /*  BUSCAR TEXTO POKEMON */
+  BuscarTexto: function (data, valorTexto) {
+    let findId1user = data.filter((elemento) =>
+      elemento.name.includes(valorTexto)
+    );
+    return findId1user;
   },
 
   /* ORDENAR DATA */
 
-  sortData : function(data,sortOrder){
-
-    if(sortOrder === "Ascendente"){
-          data.pokemon.sort((a, b) => {
-            if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
-            if(b.name.toLowerCase()> a.name.toLowerCase()) return 1;
-            return 0;
-          }); return data.pokemon}
-          
-    else if(sortOrder === "Descendente"){
-          data.pokemon.sort((a, b) => {
-            if(b.name.toLowerCase() < a.name.toLowerCase()) return -1;
-            if(a.name.toLowerCase()> b.name.toLowerCase()) return 1;
-            return 0;
-          }); return data.pokemon}
-          
-    else if(sortOrder==="Debilidad"){
+  sortData: function (data, sortOrder) {
+    if (sortOrder === "Ascendente") {
       data.pokemon.sort((a, b) => {
-        if(a.weaknesses < b.weaknesses) return -1;
-        if(b.weaknesses > a.weaknesses) return 1;
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if (b.name.toLowerCase() > a.name.toLowerCase()) return 1;
         return 0;
-      }); return data.pokemon}
-    
-    else if(sortOrder==="Resistencia"){
+      });
+      return data.pokemon;
+    } else if (sortOrder === "Descendente") {
       data.pokemon.sort((a, b) => {
-        if(b.resistant > a.resistant) return -1;
-        if(a.resistant < b.resistant) return 1;
+        if (b.name.toLowerCase() < a.name.toLowerCase()) return -1;
+        if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
         return 0;
-      }); return data.pokemon}
-    }};
+      });
+      return data.pokemon;
+    } else if (sortOrder === "Debilidad") {
+      data.pokemon.sort((a, b) => {
+        if (a.weaknesses < b.weaknesses) return -1;
+        if (b.weaknesses > a.weaknesses) return 1;
+        return 0;
+      });
+      return data.pokemon;
+    } else if (sortOrder === "Resistencia") {
+      data.pokemon.sort((a, b) => {
+        if (b.resistant > a.resistant) return -1;
+        if (a.resistant < b.resistant) return 1;
+        return 0;
+      });
+      return data.pokemon;
+    }
+  },
+};
 
-    
-export default funciones
+export default funciones;
--- src/main.js
+++ src/main.js
@@ -1,171 +1,152 @@
-import funciones from './data.js';
-import data from './data/pokemon/pokemon.js';
+import funciones from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
 const lista = document.getElementById("lista");
 
-const modalMode = document.getElementById('modal-mode'); // trae el div de la pantalla modal 
-const modalWindow = document.getElementById('modal-window');  
+const modalMode = document.getElementById("modal-mode"); // trae el div de la pantalla modal
+const modalWindow = document.getElementById("modal-window");
 
 /* FUNCIÓN DE APOYO */
 
-function mostrarCard(array){
-        array.forEach(function(element){
-        let node = document.createElement("figure");
-        let node2 = document.createElement("img");
-        let node3 = document.createElement("figcaption");
-        let animacion = document.createElement("div");
+function mostrarCard(array) {
+    console.log(array.length)
+  if (array.length !== 0) {
+    array.forEach(function (element) {
+      let node = document.createElement("figure");
+      let node2 = document.createElement("img");
+      let node3 = document.createElement("figcaption");
+      let animacion = document.createElement("div");
       /*   let node4 = document.createElement("a"); */
-        let node5 = document.createElement("p");
-        animacion.className = 'capa';
-        node5.innerText = element.about;
-        /* node4.innerText = element.num; */
-        node2.src = element.img;
-        node3.innerText = element.name;
-        let figura= document.getElementById("lista").appendChild(node);
-        figura.appendChild(node2);
-        figura.appendChild(node3);
-        figura.appendChild(animacion);
-        /* animacion.appendChild(node4); */
-        animacion.appendChild(node5); 
- 
-        let btnModal = node.querySelector('.capa')
-        btnModal.addEventListener('click', mostrarModal);
-        
-        function mostrarModal() {
-            modalMode.classList.toggle('hide'); 
-            modalWindow.classList.toggle('hide');
-            modalWindow.innerHTML =            
-            `<div id="div-img-modal">
+      let node5 = document.createElement("p");
+      animacion.className = "capa";
+      node5.innerText = element.about;
+      /* node4.innerText = element.num; */
+      node2.src = element.img;
+      node3.innerText = element.name;
+      let figura = document.getElementById("lista").appendChild(node);
+      figura.appendChild(node2);
+      figura.appendChild(node3);
+      figura.appendChild(animacion);
+      /* animacion.appendChild(node4); */
+      animacion.appendChild(node5);
+
+      let btnModal = node.querySelector(".capa");
+      btnModal.addEventListener("click", mostrarModal);
+
+      function mostrarModal() {
+        modalMode.classList.toggle("hide");
+        modalWindow.classList.toggle("hide");
+        modalWindow.innerHTML = `<div id="div-img-modal">
             <img src="${element.img}" alt="" id="img-pokemon-modal" class="image-modal">
-          </div> 
-          <h2 class="nombrePoke"> ${element.name} </h2>
-          <p class="nombrePoke"> Tipo </p>
-          <p class="nombrePoke"> <img src="./img/${element.type}.png"></a></p>
-          <p class="nombrePoke"> ${element.generation} </p>
-          <p class="nombrePoke"> ${element.height} </p>
-          <p class="nombrePoke"> ${element.weight} </p>
-          <p class="nombrePoke"> Debilidades </p>
-          <p class="nombrePoke"> ${element.weaknesses} </p>`;
+            </div> 
+            <h2 class="nombrePoke"> ${element.name} </h2>
+            <p class="nombrePoke"> ${element.generation} </p>
          
+            <p class="Characters"> Height </p>
+            <p class="Characters"> Weight </p>
+            <p class="Characters"> Tipo </p>
+          
+            <img src="/img/${element.type}.png" class="icono">
+            <p class="Characters"> ${element.height} </p>
+            <p class="Characters"> ${element.weight} </p>
+            <p class="Characters"> ${element.weaknesses} </p>
+            <p class="Characters"> ${element.resistant} </p>
+            <p class="Characters"> ${element.baseAttack} </p>
+            <p class="Characters"> ${element.baseDefense} </p>
+            <p class="Characters"> ${element.baseStamina} </p>
+            <p class="Characters"> ${element.maxCp} </p>
+            <p class="Characters"> ${element.maxHp} </p>
+           `;
 
-            const btnCloseModal = modalWindow.querySelector('#img-pokemon-modal'); 
- 
-            btnCloseModal.addEventListener('click', cerrarModal);
+        const btnCloseModal = modalWindow.querySelector("#img-pokemon-modal");
+
+        btnCloseModal.addEventListener("click", cerrarModal);
+      }
+
+      function cerrarModal() {
+        modalMode.classList.toggle("hide");
+        modalWindow.classList.toggle("hide");
+      }
+    });
+  } else {
+    lista.innerHTML = "<h1>no se ha encontrado pokemos</h1>";
+  }
 }
-
- 
-
-function cerrarModal() {
-    modalMode.classList.toggle('hide');
-    modalWindow.classList.toggle('hide');
-}
-        
-    })}
-
 
 /* FILTRO TIPO DE POKEMON */
 
 const botones = document.querySelectorAll(".fa");
 const elegir = (evento) => {
-    lista.innerHTML = "";
-    let a = funciones.FilterData(data,evento.target.id);
-       mostrarCard(a)}; 
+  lista.innerHTML = "";
+  let a = funciones.FilterData(data, evento.target.id);
+  mostrarCard(a);
+};
 
-botones.forEach(boton => {
-    boton.addEventListener("click", elegir)
+botones.forEach((boton) => {
+  boton.addEventListener("click", elegir);
 });
 
 /* ORDEN A-Z */
 
 const ordera = document.querySelector("#selecta");
-ordera.addEventListener('change', (event) => {
-    lista.innerHTML = "";
-    let valor = event.target.value;
-    let a = funciones.sortData(data,valor);
-    mostrarCard(a)
-     }); 
-const ordera2  = document.querySelector("#selecta1");
-ordera2.addEventListener('change', (event) => {
-    lista.innerHTML = "";
-    let valor = event.target.value;
-    let a = funciones.sortData(data,valor);
-    mostrarCard(a)
-     }); 
-     
-  /* BUSQUEDA POR TEXTO */
-  const text = document.querySelector("#text");
+ordera.addEventListener("change", (event) => {
+  lista.innerHTML = "";
+  let valor = event.target.value;
+  let a = funciones.sortData(data, valor);
+  mostrarCard(a);
+});
+const ordera2 = document.querySelector("#selecta1");
+ordera2.addEventListener("change", (event) => {
+  lista.innerHTML = "";
+  let valor = event.target.value;
+  let a = funciones.sortData(data, valor);
+  mostrarCard(a);
+});
+
+/* BUSQUEDA POR TEXTO */
+const text = document.querySelector("#text");
 const filtrar = () => {
-   lista.innerHTML = "";
-   const valorTexto = text.value.toLowerCase();
-   let a = funciones.AllData(data);
-   console.log(a)
-   for(let i=0;i<a[1].length;i++){
-   let nombre = a[1][i].toLowerCase();
-        if(nombre.indexOf(valorTexto) !==-1){            
-            let node = document.createElement("figure");
-            let node2 = document.createElement("img");
-            let node3 = document.createElement("figcaption");
-            let animacion = document.createElement("div");
-            let node5 = document.createElement("p");
-           /*  let node4 = document.createElement("a"); */
-            animacion.className = 'capa';
-            node.className = "fig"
-            node5.innerText = a[2][i];
-           /*  node4.innerText = a[3][i]; */
-            node2.src = a[0][i];
-            node3.innerText = a[1][i];
-            let figura= document.getElementById("lista").appendChild(node);
-            figura.appendChild(node2);
-            figura.appendChild(node3);
-            figura.appendChild(animacion);
-            /* animacion.appendChild(node4); */
-            animacion.appendChild(node5);
-        }
-    if(lista.innerHTML === ""){
-        lista.innerHTML = "<p></p>"
-    }}};
+  lista.innerHTML = "";
+  const valorTexto = text.value.toLowerCase();
+  let a = funciones.BuscarTexto(data.pokemon, valorTexto);
+  mostrarCard(a);
+};
 
-    text.addEventListener("keyup", filtrar);
-    filtrar();
-
+text.addEventListener("keyup", filtrar);
+filtrar();
 
 /* Ingresar a la segunda pantalla */
 const entrar = document.getElementById("entrar");
 
-entrar.addEventListener("click", ()=>{
-    document.getElementById("firstscreen").classList.add("hide");
-    document.getElementById("firstscreen").classList.remove("display");     
-    document.getElementById("secondscreen").classList.add("display");
-    document.getElementById("secondscreen").classList.remove("hide");
-    document.body.style.background = "#fff";
-   
-}); 
- 
+entrar.addEventListener("click", () => {
+  document.getElementById("firstscreen").classList.add("hide");
+  document.getElementById("firstscreen").classList.remove("display");
+  document.getElementById("secondscreen").classList.add("display");
+  document.getElementById("secondscreen").classList.remove("hide");
+  document.body.style.background = "#fff";
+});
+
 /* Menú desplegable */
 
 const menu = document.getElementById("menu");
-menu.addEventListener("click", function press2(){
-    let siteNav = document.getElementById("site-nav");
-        siteNav.classList.toggle("site-nav-open");
-        menu.classList.toggle("menu-open");
+menu.addEventListener("click", function press2() {
+  let siteNav = document.getElementById("site-nav");
+  siteNav.classList.toggle("site-nav-open");
+  menu.classList.toggle("menu-open");
 });
 
-
-
-    //Audio de Pokemon
-const audio = document.getElementById('audio');
-const playPauseBTN = document.getElementById('playPauseBTN');
+//Audio de Pokemon
+const audio = document.getElementById("audio");
+const playPauseBTN = document.getElementById("playPauseBTN");
 let count = 0;
-playPauseBTN.addEventListener("click", function playPause(){
-    if(count == 0){
-        count = 1;
-        audio.play();
-        playPauseBTN.innerHTML = "Pause &#9208;";
-    }else{
-        count = 0;
-        audio.pause();
-        playPauseBTN.innerHTML = "Play &#9658;";
-    }
+playPauseBTN.addEventListener("click", function playPause() {
+  if (count == 0) {
+    count = 1;
+    audio.play();
+    playPauseBTN.innerHTML = "Pause &#9208;";
+  } else {
+    count = 0;
+    audio.pause();
+    playPauseBTN.innerHTML = "Play &#9658;";
+  }
 });
-
- 
