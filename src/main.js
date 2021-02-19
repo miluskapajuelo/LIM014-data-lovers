b6d7697e8a226db170393104789caa4474983427
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-02-19 18:18:05 -0500

    modal avance

--- src/main.js
+++ src/main.js
@@ -15,18 +15,15 @@ function mostrarCard(array) {
       let node2 = document.createElement("img");
       let node3 = document.createElement("figcaption");
       let animacion = document.createElement("div");
-      /*   let node4 = document.createElement("a"); */
       let node5 = document.createElement("p");
       animacion.className = "capa";
       node5.innerText = element.about;
-      /* node4.innerText = element.num; */
       node2.src = element.img;
       node3.innerText = element.name;
       let figura = document.getElementById("lista").appendChild(node);
       figura.appendChild(node2);
       figura.appendChild(node3);
       figura.appendChild(animacion);
-      /* animacion.appendChild(node4); */
       animacion.appendChild(node5);
 
       
@@ -69,21 +66,49 @@ function mostrarCard(array) {
                 <span class="image-size">Type: </span><span id="value-height">${element.type.length}<img src="./img/${element.type[1]}.png" alt="" class="icon-medium"><img src="./img/${element.type[0]}.png" alt="" class="icon-medium"></span>
             </div> 
             </div>
-            
             </section>
-        
-            <p class="Characters"> ${element.weight} </p>
-            <p class="Characters"> ${element.weaknesses} </p>
-            <p class="Characters"> ${element.resistant} </p>
-            <p class="Characters"> ${element.baseAttack} </p>
-            <p class="Characters"> ${element.baseDefense} </p>
-            <p class="Characters"> ${element.baseStamina} </p>
-            <p class="Characters"> ${element.maxCp} </p>
-            <p class="Characters"> ${element.maxHp} </p>
-           `;
+            <section id="body-modal2" class="modal2 flex-wrap2">
+            <div class="box1">
+                <p class="titulo" >Resistant: </p>
+                <p class="caract">${element.resistant}</P>
+            </div>
+            <div class="box1">
+              <p class="titulo" >Weaknesses: </p>
+              <p class="caract">${element.weaknesses}</P>
+            </div>
+            </section>
+            
+            <div id="move-and-attack" class="column-4 flex-wrap" style="display: flex;">
+            <span class="two-fraction font f-small f-blue">stats</span>
+            <table id="stats-table" class="column-4 font">
+            <tbody><tr><th></th>
+            <th><img class="icon-small" src="./img/stats.png"></th></tr></tbody>
+            <tbody><tr>
+            <td>base-attack</td>
+            <td>${element.baseAttack} </td></tr></tbody>
+            <tbody><tr>
+            <td>base-defense</td>
+            <td>${element.baseDefense}</td></tr></tbody>
+            <tbody><tr>
+            <td>base-stamina</td>
+            <td>${element.baseStamina}</td></tr></tbody>
+            <tbody><tr>
+            <td>max-cp</td>
+            <td>${element.maxCp}</td></tr></tbody>
+            <tbody><tr>
+            <td>max-hp</td>
+            <td>${element.maxHp}</td></tr></tbody>
+          </table>
+        </div>
+            <div class="column-3 flex-wrap3 color-container3">
+              <div class="font f-small">
+                <span class="block f-blue">baseAttack:</span><br><span id="value-region" class="color-gray">${element.baseAttack}</span>
+              </div>
+            </div>
+            </section>`;
         }
         else{
-          /* console.log(element.type) */
+          
           modalWindow.innerHTML = `<div id="div-img-modal">
             <img src="${element.img}" alt="" id="img-pokemon-modal" class="image-modal">
             </div> 
@@ -117,15 +142,45 @@ function mostrarCard(array) {
             
             </section>
                      
-            <p class="Characters"> ${element.weight} </p>
-            <p class="Characters"> ${element.weaknesses} </p>
-            <p class="Characters"> ${element.resistant} </p>
-            <p class="Characters"> ${element.baseAttack} </p>
-            <p class="Characters"> ${element.baseDefense} </p>
-            <p class="Characters"> ${element.baseStamina} </p>
-            <p class="Characters"> ${element.maxCp} </p>
-            <p class="Characters"> ${element.maxHp} </p>
-           `;
+            <section id="body-modal2" class="modal2 flex-wrap2">
+            <div class="left-stat">
+              <div class="box1">
+                <p class="titulo" >Resistant: </p>
+                <p class="caract">${element.resistant}</P>
+              </div>
+              <div class="box1">
+              <p class="titulo" >Weaknesses: </p>
+              <p class="caract">${element.weaknesses}</P>
+              </div>
+            </div>
+            </div>
+            <div class="right-stat"> 
+             <div id="move-and-attack" class="column-4 flex-wrap" style="display: flex;">
+             <span class="two-fraction font f-small f-blue">stats</span>
+             <table id="stats-table" class="column-4 font">
+             <tbody><tr><th></th>
+             <th><img class="icon-small" src="./img/stats.png"></th></tr></tbody>
+             <tbody><tr>
+             <td>base-attack</td>
+             <td>${element.baseAttack} </td></tr></tbody>
+             <tbody><tr>
+             <td>base-defense</td>
+             <td>${element.baseDefense}</td></tr></tbody>
+             <tbody><tr>
+             <td>base-stamina</td>
+             <td>${element.baseStamina}</td></tr></tbody>
+             <tbody><tr>
+             <td>max-cp</td>
+             <td>${element.maxCp}</td></tr></tbody>
+             <tbody><tr>
+             <td>max-hp</td>
+             <td>${element.maxHp}</td></tr></tbody>
+             </table>
+            </div>
+            </div>
+            </section>
+            `;
+
         }
         const btnCloseModal = modalWindow.querySelector("#img-pokemon-modal");
 
@@ -140,9 +195,34 @@ function mostrarCard(array) {
   } else {
     lista.innerHTML = "<h1>no se ha encontrado pokemos</h1>";
   }
-}
-/* ${element.type} */
+} 
+
 /* FILTRO TIPO DE POKEMON */
+
+/* GRAFICO RADIAL */
+/* var marksCanvas = document.getElementById("marksChart");
+
+var marksData = {
+  labels: ["English", "Maths", "Physics", "Chemistry", "Biology", "History"],
+  datasets: [{
+    label: "Student A",
+    backgroundColor: "rgba(200,0,0,0.2)",
+    data: [65, 75, 70, 80, 60, 80]
+  }, {
+    label: "Student B",
+    backgroundColor: "rgba(0,0,200,0.2)",
+    data: [54, 65, 60, 70, 70, 75]
+  }]
+};
+
+var radarChart = new Chart(marksCanvas, {
+  type: 'radar',
+  data: marksData
+}); 
+
+return radarChart */
+  
+
 
 const botones = document.querySelectorAll(".fa");
 const elegir = (evento) => {
@@ -154,7 +234,7 @@ const elegir = (evento) => {
 botones.forEach((boton) => {
   boton.addEventListener("click", elegir);
 });
-
+ 
 /* ORDEN A-Z */
 
 const ordera = document.querySelector("#selecta");
@@ -170,7 +250,7 @@ ordera2.addEventListener("change", (event) => {
   let valor = event.target.value;
   let a = funciones.sortData(data, valor);
   mostrarCard(a);
-});
+}); 
 
 /* BUSQUEDA POR TEXTO */
 const text = document.querySelector("#text");
@@ -181,7 +261,7 @@ const filtrar = () => {
   mostrarCard(a);
 };
 text.addEventListener("keyup", filtrar);
-filtrar();
+filtrar(); 
 
 /* Ingresar a la segunda pantalla */
 const entrar = document.getElementById("entrar");
@@ -192,7 +272,7 @@ entrar.addEventListener("click", () => {
   document.getElementById("secondscreen").classList.add("display");
   document.getElementById("secondscreen").classList.remove("hide");
   document.body.style.background = "#fff";
-});
+}); 
 
 /* Menú desplegable */
 
@@ -201,7 +281,7 @@ menu.addEventListener("click", function press2() {
   let siteNav = document.getElementById("site-nav");
   siteNav.classList.toggle("site-nav-open");
   menu.classList.toggle("menu-open");
-});
+}); 
 
 //Audio de Pokemon
 const audio = document.getElementById("audio");
@@ -217,4 +297,4 @@ playPauseBTN.addEventListener("click", function playPause() {
     audio.pause();
     playPauseBTN.innerHTML = "Play &#9658;";
   }
-});
+}); 
