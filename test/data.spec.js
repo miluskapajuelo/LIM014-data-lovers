73df1bf03d66e529cf33d77a3bb6163f4fc436c3
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-02-26 12:48:01 -0500

    se avanzan los test 50%

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,19 +1,26 @@
-import {} from '../src/data.js';
+import {funciones} from '../src/data.js';
 
 /* BUSCAR POKEMONES */
-/* describe('funciones', () => {
+/*  */
+
+  describe('funciones', () => {
+
+    // Prueba 1 : ¿Es función?
   it('is a function', () => {
     expect(typeof example).toBe('object');
   });
- 
-
+});
+  // Prueba 1 : ¿Es función?
 describe('funciones.FilterData', () => {
+  // Prueba 1 : ¿Es función?
   it('is a function', () => {
-    expect(typeof funciones.FilterData(data, valorTexto)).toBe('function');
+    expect(typeof funciones.FilterData).toBe('function');
   });
 
+  // Prueba 2 :Filtrar data
   it('returns `It should return "grass"`', () => {
-    expect(funciones.FilterData([{
+
+    const data = [{
       "num": "001",
       "name": "bulbasaur",
       "generation": {
@@ -23,26 +30,50 @@ describe('funciones.FilterData', () => {
       "pokemon-rarity": "normal",
       "type": [
         "grass",
-    ]}],"grass")).toBe('true');
+    ]}]
+
+    const result = true;
+    const type = "grass"
+
+    expect(funciones.FilterData(data,type)).toBe(result);
   });
+
+
 });
 
 describe('funciones.sortData', () => {
+  // Prueba 1 : ¿Es función?
   it('is a function', () => {
-    expect(typeof funciones.sortData(data, sortOrder)).toBe('function');
+    expect(typeof funciones.sortData).toBe('function');
   });
 
+  // Prueba 2 : Ordenar Data
   it('returns `It should return "grass"`', () => {
-    expect(funciones.FilterData([{
-      "num": "001",
-      "name": "bulbasaur",
-      "generation": {
-        "num": "generation i",
-        "name": "kanto"
-      },
-      "pokemon-rarity": "normal",
-      "type": [
-        "grass",
-    ]}])).toBe('grass');
-  });
-});  */
+
+   
+    const data = [{
+      name: 'A',
+    },
+    {
+      name: 'Z',
+    },
+    {
+      name: 'D',
+    }
+  ];
+  const result = [{
+      name: 'A',
+    },
+    {
+      name: 'D',
+    },
+    {
+      name: 'Z',
+    }
+  ];
+
+    const order = "Ascendente";
+
+    expect(funciones.sortData(data,order)).toBe(result);
+  })
+});
